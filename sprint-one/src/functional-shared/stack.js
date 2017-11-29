4988630c6a2043aaff8d75a5591dc7ea247f9837
dust-off <dustinburns@gmail.com>
2017-11-29 15:28:40 -0800

    functional shared stack completed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.data = {};
+  newStack.counter = 0;
+  extend(newStack, stackMethods);
+  return newStack;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var stackMethods = {
+  size : function() {
+    return this.counter;
+  },
+  
+  push : function(value) {
+    this.data[this.counter] = value;
+    this.counter++;
+  },
+  
+  pop : function() {
+    var popped;
+    if (this.counter > 0) {
+      this.counter--;
+      popped = this.data[this.counter];
+    }
+    return popped;
+  }
+};
 
 
