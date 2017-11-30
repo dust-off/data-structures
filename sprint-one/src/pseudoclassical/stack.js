13416a0e03f41b2cdf7fd943f2dac4ec1a803f97
dust-off <dustinburns@gmail.com>
2017-11-29 16:19:27 -0800

    Psudoclassical stack working for all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.lineCook = {};
+  this.lastDudeInLine = 0;
 };
 
+Stack.prototype.size = function() {
+  return this.lastDudeInLine;
+};
 
+Stack.prototype.push = function(order) {
+  this.lineCook[this.lastDudeInLine] = order;
+  this.lastDudeInLine++;
+};
+
+Stack.prototype.pop = function() {
+  var order;
+  if (this.lastDudeInLine > 0) {
+    this.lastDudeInLine--;
+    order = this.lineCook[this.lastDudeInLine];
+    delete this.lineCook[this.lastDudeInLine];
+  }
+  return order;
+};
