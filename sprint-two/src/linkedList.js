80bd3152dba40ec69d9522cb6c9b646192ca6e15
dust-off <dustinburns@gmail.com>
2017-11-29 19:45:51 -0800

    linkedList works

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var formerHead = list.head;
+    list.head = list.head.next;
+    return formerHead.value;
   };
 
   list.contains = function(target) {
+    //function that takes in a node
+    var found = false;
+    var checker = function(node) {
+      if (node.value === target) {
+        found = true;
+        return true;
+      } else if (node.next !== null) {
+        checker(node.next);
+      } else {
+        return false;
+      }
+    };
+    checker(list.head);
+    return found;
   };
 
   return list;
