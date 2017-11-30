6527c0b74e936138f4db2e56e85f7de84e00ef83
dust-off <dustinburns@gmail.com>
2017-11-29 20:12:39 -0800

    Started on tree with several passing tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,19 +4,26 @@ var Tree = function(value) {
 
   // your code here
   newTree.children = null;  // fix me
-
+  extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children = [Tree(value)];
 };
 
 treeMethods.contains = function(target) {
+  var truthy = false;
 };
 
 
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
