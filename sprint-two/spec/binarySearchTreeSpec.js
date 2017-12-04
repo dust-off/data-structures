011d82a1c303c8b1bf3292fe0b7a5f51419a534f
Dustin Burns <dustinburns@gmail.com>
2017-12-03 16:36:16 -0800

    more copies

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -1,40 +1,102 @@
-describe('binarySearchTree', function() {
-  var binarySearchTree;
+var BinarySearchTree = function(value) {
+  var tracker;
+  var obj = {
+    value: value,
+    leftCounter: 0,
+    rightCounter: 0,
+    left: {
+      //value: 4
+      //depth: 0 + if(parent + 1);
+      //left: {}
+      //right: {}
+    },
+    right: {
+      //value: 6
+      //left: {}
+      //right: {}
+    }
+  };
+  
+  extend(obj, binarySearchTreeMethods);
+  console.log(obj);
+  return obj;
+};
 
-  beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
-  });
+//a method is just a function bound to an object?
+var binarySearchTreeMethods = {};
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
-    expect(binarySearchTree.insert).to.be.a('function');
-    expect(binarySearchTree.contains).to.be.a('function');
-    expect(binarySearchTree.depthFirstLog).to.be.a('function');
-  });
+binarySearchTreeMethods.insert = function(value) {
+  //decides if it should add to left or right
+  //set unbalanced after the ++
+  //if unblanced === me.value then rebalance
+  var highestUnblanced = this.onBalance(this);
+  if (highestUnblanced !== undefined) {
+    //rebalancer.call(this, highestUnbalanced);
+  }
+  if (value === this.value) {
+    return false;
+  } else if (this.value < value) {
+    if (this.right.value !== undefined) {
+      this.rightCounter ++;
+      this.right.insert(value);  
+    } else {
+      this.rightCounter ++;    
+      this.right = BinarySearchTree(value);
+    }
+  } else {
+    if (this.left.value !== undefined) {
+      this.leftCounter ++;
+      this.left.insert(value);  
+    } else {
+      this.leftCounter = this.leftCounter + 1;
+      this.left = BinarySearchTree(value);
+    }
+  }
+};
 
-  it('should insert values at the correct location in the tree', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(6);
-    expect(binarySearchTree.left.right.value).to.equal(3);
-    expect(binarySearchTree.right.left.value).to.equal(6);
-  });
+binarySearchTreeMethods.onBalance = function(node) {
+  if (node.right.value / node.left.value >= 2 || node.left.value / node.right.value >= 2) {
+    return node.value;
+  }
+};
 
-  it('should have a working "contains" method', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    expect(binarySearchTree.contains(7)).to.equal(true);
-    expect(binarySearchTree.contains(8)).to.equal(false);
-  });
+binarySearchTreeMethods.contains = function(findThis) {
+  if (this.value === findThis) {
+    return true;
+  } else {
+    if (findThis < this.value) {
+      return this.contains.call(this.left, findThis);
+    } else if (findThis > this.value) {
+      return this.contains.call(this.right, findThis);
+    }
+  }
+  return false;
+};
 
-  it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
-    var array = [];
-    var func = function(value) { array.push(value); };
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([5, 2, 3, 7]);
-  });
-});
+binarySearchTreeMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left.value !== undefined) {
+    this.depthFirstLog.call(this.left, cb);
+  } 
+  if (this.right.value !== undefined) {
+    this.depthFirstLog.call(this.right, cb);
+  }
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ 
+ - insert : O(log(n))
+ 
+ - contains : O(log(n))
+ 
+ - depthFirst : linear
+  
+ */
+
